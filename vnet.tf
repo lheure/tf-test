2e5551bbf4dafd9fb8ba776384f3da0007a48908
chwan.yoo <example@mail.com>
2018-03-25 13:44:57 +0900

    1344

--- vnet.tf
+++ vnet.tf
@@ -3,7 +3,7 @@ resource "azurerm_virtual_network" "tfvnet" {
     name                = "${var.prefix}-vnet"
     address_space       = ["10.200.0.0/16"]
     location            = "${var.location}"
-    resource_group_name = "${azurerm_resource_group.${var.prefix}.name}"
+    resource_group_name = "${azurerm_resource_group.vision-poc.name}"
 
     tags {
         environment = "${var.tag}"
