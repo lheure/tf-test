8504a42281ee71e717090e005fce1bceed599a56
chwan.yoo <example@mail.com>
2018-03-25 14:52:52 +0900

    .

--- vnet.tf
+++ vnet.tf
@@ -9,3 +9,15 @@ resource "azurerm_virtual_network" "tfvnet" {
         environment = "${var.tag}"
     }
 }
+
+# Create virtual network
+resource "azurerm_virtual_network" "tfvnet" {
+    name                = "${var.prefix}-vnet"
+    address_space       = ["20.200.0.0/16"]
+    location            = "${var.location}"
+    resource_group_name = "${azurerm_resource_group.vision-poc.name}"
+
+    tags {
+        environment = "${var.tag}"
+    }
+}
